dbe31b631c9815545d0d0a41f9702ad532dfbb05
MKdigiworld <mkdigiworld01@gmail.com>
2026-09-07 14:09:19 +0500

    Delete app/app directory

--- app/app/page.tsxpage.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-          <main style={{ padding: '2rem', textAlign: 'center' }}>
-                  <h1>Welcome to ProDocsTool</h1>h1>
-                <p>Modern PDF Tools & Invoice Creator</p>p>
-                <p>Coming Soon...</p>p>
-          </main>main>
-        )
-}</h1>
