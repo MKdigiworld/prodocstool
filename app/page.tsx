eeb47db95a67dd9164bd1512f9708299dc876c2f
MKdigiworld <mkdigiworld01@gmail.com>
2026-09-07 13:07:40 +0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+          <main style={{ padding: '2rem', textAlign: 'center' }}>
+                  <h1>Welcome to ProDocsTool</h1>h1>
+                <p>Modern PDF Tools & Invoice Creator</p>p>
+                <p style={{ marginTop: '2rem', fontSize: '18px' }}>Coming Soon...</p>p>
+          </main>main>
+        )
+}</h1>
